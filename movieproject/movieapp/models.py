9c26c6878f19bf07009afdb089cb9a20e9bf9563
nahas006 <143589242+nahas006@users.noreply.github.com>
2023-09-11 13:00:10 +0530

    added

---
+++ movieproject/movieapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.http import HttpResponse
+
+
+# Create your models here.
+class Movie(models.Model):
+    name=models.CharField(max_length=250)
+    desc=models.TextField()
+    year=models.IntegerField()
+    img=models.ImageField(upload_to='gallery')
+
+    def __str__(self):
+        return  self.name
+
